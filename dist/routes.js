adbfe41855fb2f2131c5989f282d1c13e4740ab0
Sammuel Reis <sammugr@hotmail.com>
2020-06-01 20:52:11 -0300

    implements createUser func
    implements a new function to go deeper into types with typescript :alien:

--- dist/routes.js
+++ dist/routes.js
@@ -1,7 +1,21 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.helloWorld = void 0;
+var createUser_1 = __importDefault(require("./services/createUser"));
 function helloWorld(req, res) {
+    var user = createUser_1.default({
+        email: 'sammuel@email.com.br',
+        password: '123456',
+        techs: [
+            'Node.js',
+            'ReactJS',
+            'React Native',
+            { title: 'JavaScript', experience: 100 }
+        ]
+    });
     return res.json({ message: 'Hello World' });
 }
 exports.helloWorld = helloWorld;
--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,17 @@
 import { Request, Response } from 'express';
+import createUser from './services/createUser';
 
 export function helloWorld(req: Request, res: Response) {
+  const user = createUser({
+    email: 'sammuel@email.com.br',
+    password: '123456',
+    techs: [
+      'Node.js',
+      'ReactJS',
+      'React Native',
+      { title: 'JavaScript', experience: 100 }
+    ]
+  });
+  
   return res.json({ message: 'Hello World' });
 }
